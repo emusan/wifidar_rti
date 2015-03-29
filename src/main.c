550fb2f315a0739d7dd76ee5297c54d0f67912b3
Tom McLeod <emus.attack@gmail.com>
2015-03-29 00:00:34 -0400

    Fix a few minor issues and clean up the code a bit. Now properly calculates the FFT :)

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <fftw3.h>
-#include<string.h>
-#include<stdlib.h>
-#include"plplot.h"
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include "plplot.h"
 
 #define N 1000
 
@@ -10,14 +11,14 @@ int main() {
 	// file I/O variables
 	FILE *stream = fopen("test_signal_100Hz_50kHzFs.csv","r");
 	float all_samples[300000];
-	float samples_section[250][1000];
+	float samples_section[250][N];
 	char *curr_num;
 	int i,j;
 	char line[3000000];
 
 	// plplot variables
-	PLFLT x[1000],y[1000];
-	PLFLT xmin = 0.,xmax = 1000.,ymin = -1.,ymax = 1.;
+	PLFLT x[N],y[N];
+	PLFLT xmin = 0.,xmax = N,ymin = -1.,ymax = 1.;
 
 	// fftw variables
 	fftw_complex *freq_domain_array;
@@ -27,7 +28,12 @@ int main() {
 	// fftw init stuff
 	time_domain_array = (double*) fftw_malloc(sizeof(double) * N);
 	freq_domain_array = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * ((N/2)+1));
-	fft_plan = fftw_plan_dft_r2c_1d(N,time_domain_array,freq_domain_array,FFTW_MEASURE);
+	fft_plan = fftw_plan_dft_r2c_1d(N,time_domain_array,freq_domain_array,FFTW_ESTIMATE);
+	if(0 == fft_plan)
+	{
+		printf("there has been a problem creating the plan :(");
+		return 1;
+	}
 
 	// grab the info from the file
 	fgets(line,3000000,stream);
@@ -45,7 +51,7 @@ int main() {
 
 	for(i = 0;i < 250;i++)
 	{
-		for(j = 0;j < 1000;j++)
+		for(j = 0;j < N;j++)
 		{
 			samples_section[i][j] = all_samples[(i+1)*j];
 			//printf("%f ",samples_section[i][j]);
@@ -53,7 +59,7 @@ int main() {
 		//printf("\n");
 	}
 
-	for(j = 0;j < 1000;j++)
+	for(j = 0;j < N;j++)
 	{
 		x[j] = j;
 		y[j] = (PLFLT) samples_section[1][j];
@@ -67,23 +73,22 @@ int main() {
 	pladv(1);
 	pllab("x","y = sin(2*pi*100*x)","Simple sine plot");
 
-	plline(1000,x,y);
+	plline(N,x,y);
 
-	for(i = 0;i<1000;i++)
+	for(i = 0;i<N;i++)
 	{
 		time_domain_array[i] = (double) y[i];
 	}
 	fftw_execute(fft_plan);
-	for(i = 0;i<1000;i++)
+	for(i = 0;i<N/2;i++)
 	{
-		y[i] = freq_domain_array[i][0];
+		y[i] = sqrt((freq_domain_array[i][0] * freq_domain_array[i][0]) + (freq_domain_array[i][1] * freq_domain_array[i][1]));
 		printf("%f\n",y[i]);
 	}
 
-	plenv(xmin,xmax,ymin,ymax,0,0);
-	pladv(2);
+	plenv(xmin,xmax/2,ymin,ymax,0,0);
 	pllab("x","y = FFT(y)","FFT of previous sine wave");
-	plline(1000,x,y);
+	plline(xmax/2,x,y);
 
 	fftw_destroy_plan(fft_plan);
 	fftw_free(time_domain_array);fftw_free(freq_domain_array);
