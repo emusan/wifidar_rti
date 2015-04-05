8f5eda8e4023cae65b050e8146a1ced94732535b
Tom McLeod <emus.attack@gmail.com>
2015-04-04 22:08:58 -0400

    Actually fixes the bug, due to lack of parenthesis around a define statement.

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 #define RAMP_FREQ 100
 #define SAMPLES_PER_WAVEFORM SAMPLE_RATE/RAMP_FREQ
 #define WAVEFORM_BUFFER_SIZE 30
-#define SAMPLES_RTI SAMPLES_PER_WAVEFORM/2 + 1
+#define SAMPLES_RTI (SAMPLES_PER_WAVEFORM/2 + 1)
 
 void retrieve_samples_csv(FILE *stream,int num_samples,float *input_samples);
 void amplitude(fftw_complex *complex_array,double *amplitude_array,int length);
@@ -38,9 +38,9 @@ int main()
 	fftw_plan fft_plan;
 
 	// fftw init stuff
-	time_domain_array = (double *) fftw_malloc(sizeof(double) * SAMPLES_PER_WAVEFORM);
-	freq_domain_array = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * SAMPLES_RTI);
-	fft_plan = fftw_plan_dft_r2c_1d(SAMPLES_PER_WAVEFORM,time_domain_array,freq_domain_array,FFTW_ESTIMATE);
+	time_domain_array = (double*) fftw_malloc(sizeof(double) * SAMPLES_PER_WAVEFORM);
+	freq_domain_array = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * (SAMPLES_RTI));
+	fft_plan = fftw_plan_dft_r2c_1d(SAMPLES_PER_WAVEFORM,time_domain_array,freq_domain_array,FFTW_PATIENT);
 	if(0 == fft_plan)
 	{
 		printf("there has been a problem creating the plan :(");
@@ -52,6 +52,7 @@ int main()
 	plinit();
 	plssub(2,1);
 	plenv(time_xmin,time_xmax,time_ymin,time_ymax,0,0);
+	pllab("x","y = sin(2*pi*100*x)","Simple sine plot");
 	plenv(rti_xmin,rti_xmax,rti_ymin,rti_ymax,0,0);
 
 	plAlloc2dGrid(&rti_z,rti_xint,rti_yint);
@@ -66,10 +67,8 @@ int main()
 	fgets(line,1000000,stream);
 	curr_num = strtok(line,",");
 
-	int rti_running = 1;
-	j = 0;
 
-	while(1 == rti_running)
+	for(j = 0;j < WAVEFORM_BUFFER_SIZE;j++)
 	{
 		// take in samples
 		for(curr_num = strtok(NULL,","),i=0; curr_num != NULL && i<SAMPLES_PER_WAVEFORM; curr_num = strtok(NULL,","),i++)
@@ -85,16 +84,16 @@ int main()
 			time_y[i] = (PLFLT) input_samples[i];
 		}
 		pladv(1);
-		pllab("x","y = sin(2*pi*100*x)","Simple sine plot");
 
 		plline(SAMPLES_PER_WAVEFORM,time_x,time_y);
 
 		// take FFT
 		for(i = 0;i<SAMPLES_PER_WAVEFORM;i++)
 		{
-			time_domain_array[i] = (double) time_y[i];
+			time_domain_array[i] = (double) input_samples[i];
 		}
 		fftw_execute(fft_plan);
+		
 		amplitude(freq_domain_array,time_y,SAMPLES_RTI);
 		
 		// display on RTI
@@ -106,38 +105,17 @@ int main()
 		pladv(2);
 
 		plimage((const PLFLT * const *)(rti_z),rti_xint,rti_yint,1.0,(PLFLT) rti_xint,-30.,0.0,-1.0,550.0,1.0,(PLFLT) rti_xint,-30.,0.0);
-
-		if(WAVEFORM_BUFFER_SIZE-1 == j)
-		{
-			rti_running = 0;
-		} else {
-			j++;
-		}
+		
 	}
 
 	// clean up
+	fftw_destroy_plan(fft_plan);
+	fftw_free(time_domain_array);
+	fftw_free(freq_domain_array);
 	plFree2dGrid(rti_z,rti_xint,rti_yint);
 	plend();
-	fftw_destroy_plan(fft_plan);
-	fftw_free(freq_domain_array);
-	fftw_free(time_domain_array);
 
 	return 0;
-}
-
-// function retrieve_samples_csv
-// pulls num_samples from input csv file stream and puts them into the array specified by input_samples
-void retrieve_samples_csv(FILE *stream,int num_samples,float *input_samples)
-{
-	char line[num_samples*100];
-	char *curr_num;
-	int i;
-	fgets(line,num_samples*100,stream);
-	
-	for(curr_num = strtok(line,","),i=0; curr_num != NULL && i<num_samples; curr_num = strtok(NULL,","),i++)
-	{
-		input_samples[i] = (float) strtof(curr_num,NULL);
-	}
 }
 
 void amplitude(fftw_complex *complex_array,double *amplitude_array,int length)
